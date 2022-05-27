a50775f504c96bac17384af3fa823914e1aa5812
Vic <Kawemirs@gmail.com>
2022-05-27 05:08:34 -0400

    Update search.lisp

--- search.lisp
+++ search.lisp
@@ -1,7 +1,30 @@
+(defvar *dbg-ids* nil "Identifiers used by dbg")
+
+(defun dbg (id format-string &rest args)
+  (when (member id *dbg-ids*)
+    (fresh-line *debug-io*)
+    (apply #'format *debug-io* format-string args)))
+
+(defun debugv (&rest ids)
+  "Start dbg output on the given ids."
+  (setf *dbg-ids* (union ids *dbg-ids*)))
+
+(defun undebug (&rest ids)
+  (setf *dbg-ids* (if (null ids) nil
+                      (set-difference *dbg-ids* ids))))
+
+(defun dbg-ident (id indent format-string &rest args)
+  "Print indented debugging info if (DEBUG ID) has been specified."
+  (when (member id *dbg-ids*)
+    (fresh-line *debug-io*)
+    (dotimes (i indent) (princ "  " *debug-io*))
+    (apply #'format *debug-io* format-string args)))
+
 (defun tree-search (states goal-p successors combiner)
   "Find a state that satisfies goal-p. Start with states
   and search according to successors and combiner."
-  (cond ((null states) fail)
+  (dbg :search "~&:: Search: ~a" states)
+  (cond ((null states) nil)
         ((funcall goal-p (car states)) (car states))
         (t (tree-search
             (funcall combiner
@@ -29,7 +52,7 @@
 
 (defun sorter (cost-fn)
   "Return a combiner function that sorts according to cost-fn."
-  #'(lambda (new old) (sort (append new old) #'< :key const-fn)))
+  #'(lambda (new old) (sort (append new old) #'< :key cost-fn)))
 
 (defun prepend (x y)
   "Prepend y to start of x"
@@ -52,7 +75,7 @@
 (defun beam-search (start goal-p successor cost-fn beam-width)
   "Search highest scoring states first until goal is reached,
   but never consider more than beam-width states at a time."
-  (tree-search (list start) goal-p successors
+  (tree-search (list start) goal-p successor
                #'(lambda (old new)
                    (let ((sorted (funcall (sorter cost-fn) old new)))
                      (if (> beam-width (length sorted))
@@ -68,3 +91,123 @@
 (best-first-search 1 (is 12) #'binary-tree (price-is-right 12))
 
 (beam-search 1 (is 12) #'binary-tree (price-is-right 12) 2)
+
+(defstruct (city (:type list)) name long lat)
+
+(defparameter  *cities*
+  '((Atlanta        84.23 33.45) (Los-Angeles       118.15   34.03)
+    (Boston         71.05 42.21) (Memphis            90.03   35.09)
+    (Chicago        87.37 41.50) (New-York           73.58   40.47)
+    (Denver        105.00 39.45) (Oklahoma-City      97.28   35.26)
+    (Eugene        123.05 44.03) (Pittsburgh         79.57   40.27)
+    (Flagstaff     111.41 35.13) (Quebec               71.11 46.49)
+    (Grand-Jet     108.37 39.05) (Reno              119.49   39.30)
+    (Houston       105.00 34.00) (San-Francisco     122.26   37.47)
+    (Indianapolis   86.10 39.46) (Tampa              82.27   27.57)
+    (Jacksonville   81.40 30.22) (Victoria           123.21  48.25)
+    (Kansas-City    94.35 39.06) (Wilmington         77.57   34.14)))  
+
+(defun neighbors (city)
+  "Find all cities within 1000 kilometers."
+  (remove-if-not #'(lambda (c)
+                     (and (not (eq c city))
+                          (< (air-distance c city) 1000.0)))
+                 *cities*))
+
+(defun city (name)
+  "Find the city with this name."
+  (assoc name *cities*))
+
+;; (defun trip (start dest)
+;;   "Search for a way from the start to dest."
+;;   (beam-search start (is dest) #'neighbors
+;;                #'(lambda (c) (air-distance c dest))
+;;                1))
+
+(defconstant earth-diameter 12765.0
+  "Diameter of planet earth in kilometers.")
+
+(defun air-distance (city1 city2)
+  "The great circle distance between two cities."
+  (let ((d (distance (xyz-coords city1) (xyz-coords city2))))
+    ;; d is the straight-line chord between the two cities.
+    ;; The length of the subtending arc is given by:
+    (* earth-diameter (asin (/ d 2)))))
+
+(defun xyz-coords (city)
+  "Returns the x,y,z coordinates of a point on a sphere.
+  The center is (0 0 0) and the north pole is (0 0 1)."
+  (let ((psi (deg->radians (city-lat city)))
+        (phi (deg->radians (city-long city))))
+    (list (* (cos psi) (cos phi))
+          (* (cos psi) (sin phi))
+          (sin psi))))
+
+(defun distance (point1 point2)
+  "The Euclidean distance between two points.
+  The points are coordinates in n-dimensional space."
+  (sqrt (reduce #'+ (mapcar #'(lambda (a b) (expt (- a b) 2))
+                            point1 point2))))
+
+(defun deg->radians (deg)
+  "Convert degrees and minutes to radians."
+  (* (+ (truncate deg) (* (rem deg 1) 100/60)) pi 1/180))
+
+(defstruct (path (:print-function print-path))
+  state (previous nil) (cost-so-far 0) (total-cost 0))
+
+(defun trip (start dest &optional (beam-width 1))
+  "Search for the best path from the start to dest."
+  (beam-search
+   (make-path :state start)
+   (is dest :key #'path-state)
+   (path-saver #'neighbors #'air-distance #'(lambda (c) (air-distance c dest)))
+   #'path-total-cost
+   beam-width))
+
+(defun is (value &key (key #'identity) (test #'eql))
+  #'(lambda (path) (funcall test value (funcall key path))))
+
+(defun path-saver (successors cost-fn cost-left-fn)
+  #'(lambda (old-path)
+      (let ((old-state (path-state old-path)))
+        (mapcar
+         #'(lambda (new-state)
+             (let ((old-cost
+                     (+ (path-cost-so-far old-path)
+                        (funcall cost-fn old-state new-state))))
+               (make-path
+                :state new-state
+                :previous old-path
+                :cost-so-far old-cost
+                :total-cost (+ old-cost (funcall cost-left-fn new-state)))))
+         (funcall successors old-state)))))
+
+(defun print-path (path &optional (stream t) depth)
+  (declare (ignore depth))
+  (format stream "#<Path to ~a cost ~,1f>"
+          (path-state path) (path-total-cost path)))
+
+(defun show-city-path (path &optional (stream t))
+  "Show the length of a path, and the cities along it."
+  (format stream "#<Path ~,1f km: ~{~:(~a~)~^ - ~}>"
+          (path-total-cost path)
+          (reverse (map-path #'city-name path)))
+  (values))
+
+(defun map-path (fn path)
+  "Call fn on each state in the path, collecting results."
+  (if (null path)
+      nil
+      (cons (funcall fn (path-state path))
+            (map-path fn (path-previous path)))))
+
+(defun iter-wide-search (start goal-p successors cost-fn
+                         &key (width 1) (max 100))
+  "Search, increasing beam width from width to max.
+  Return the first solution found at any width."
+  (dbg :search ": Width: ~d" width)
+  (unless (> width max)
+    (or (beam-search start goal-p successors cost-fn width)
+        (iter-wide-search start goal-p successors cost-fn
+                          :width (+ 1 width) :max max))))
